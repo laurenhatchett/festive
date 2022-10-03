c67ca29af4720627f5752cb7475f5c1510a70242
laurenhatchett <laurenhatchett@gmail.com>
2022-10-02 21:25:32 -0400

    fix postsIndex and newPost functionality

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,52 +1,57 @@
 import { Post } from '../models/post.js'
 
+
 function postsIndex(req, res) {
   Post.find({})
   .then(posts => {
-    res.render('posts/index', {
+    res.render('posts', {
       posts,
-      title: "Add Post!"
+      title: 'Add Post!'
     })
   })
   .catch(err => {
     console.log(err)
-    res.redirect('/')
+    res.redirect('/posts/new')
   })
 }
 
-function createPost(req, res, next) {
-  req.body.owner = req.user.profile._id
-  Post.create(req.body)
-  then(post => {
-    res.redirect(`/posts`)
-  })
-  .catch(err => {
-  console.log(err)
-  res.redirect('/posts/')
+
+function newPost(req, res) {
+  res.render('posts/new', {
+    title: 'Add Post!'
   })
 }
 
-function postsShow (req, res) {
-  Post.findById(req.params.id)
-  .populate("owner")
-  .then(post => {
-    res.render('posts/index', {
-      post,
-      title: "Thanks for Posting"
-    })
-  })
-  .catch(err => {
-    console.log(err)
-    res.redirect('/posts/')
-    })
-}
+// function createPost(req, res, next) {
+//   req.body.owner = req.user.profile._id
+//   Post.create(req.body)
+//   then(post => {
+//     res.redirect(`/posts`)
+//   })
+//   .catch(err => {
+//   console.log(err)
+//   res.redirect('/posts/')
+//   })
+// }
+
+// function show (req, res) {
+//   Post.findById(req.body) //params
+//   .populate("owner")
+//   .then(post => {
+//     res.render('posts/postsShow', {
+//       post,
+//     })
+//   })
+//   .catch(err => {
+//     console.log(err)
+//     res.redirect('/posts/')
+//   })
+// }
+
+
 
 
 export {
+  newPost as new,
   postsIndex,
-  createPost,
-  postsShow,
 }
-
-
-
--- routes/posts.js
+++ routes/posts.js
@@ -1,17 +1,34 @@
 import { Router } from 'express'
+import * as postsCtrl from '../controllers/posts.js'
 import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
-import * as postsCtrl from '../controllers/posts.js'
 
 //GET localhost:3000/posts 
 router.get('/', postsCtrl.postsIndex)
 
+//GET localhost:3000/posts/new
+router.get('/new', postsCtrl.new)
+
+//GET localhost:3000
+
+
+
+
+
+
+
+
+
+
+
+
+
 //GET localhost:3000/posts/:id
-router.get('/:id', postsCtrl.postsShow)
+//router.get('/:id', postsCtrl.show) //postsShow
 
 //POST localhost:3000/posts
-router.post('/', isLoggedIn, postsCtrl.createPost)
+//router.post('/', isLoggedIn, postsCtrl.createPost)
 
 export {
   router,
